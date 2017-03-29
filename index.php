0f46a869d7d91e6bec4893025246294f89e8a9af
chris@cru.io <logicbase.acha@gmail.com>
2017-03-29 16:17:40 +0800

    Added feeds

--- index.php
+++ index.php
@@ -25,6 +25,11 @@
 		</div>	
 	</div>
 
+
+
+
+	
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script src="js/clipboard.min.js"></script>
 	<script src="js/main.js"></script>
